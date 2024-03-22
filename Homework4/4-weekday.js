8bbbe13529fe090b347a1a1e6ebebb9ef4b7a116
taitaiwu <s919075@stu.chsc.tw>
2024-03-22 11:25:31 +0800

    Homework4 add No.1~4

---
+++ Homework4/4-weekday.js
@@ -0,0 +1,17 @@
+var num = { Sunday:"0" , Monday:"1" , Tuesday:"2" , Wednesday:"3" , Thursday:"4" , Friday:"5" , Saturday:"6"}
+
+function weekday (str)
+{
+    var n=[];
+
+    for (let i in str)
+    {
+        var str_word=str[i];
+        var num_word=num[str_word];
+        n.push(num_word);
+    }
+    return n;
+}
+console.log("weekday:", Deno.args);
+var n=weekday(Deno.args);
+console.log("transformed:",n);
