5baaa1909adac37d41a05990d90be161793fc6dc
taitaiwu <s919075@stu.chsc.tw>
2024-06-13 00:02:45 +0800

    update Homework3 no.7

--- Homework3/7-matrixMul.js
+++ Homework3/7-matrixMul.js
@@ -1,14 +1,16 @@
 function matrixMul(a,b)
 {
-    var sum=[0];
-    let i,j;
+    var sum=Array(b[0].length).fill(0);
+    let i,j,k;
 
     for (i=0 ; i<a[i].length ; i++)
     {
-        let j;
         for (j=0 ; j<b[j].length ; j++)
         {
-            sum[i][j]+=a[i][j]*b[j][i];
+            for (k = 0; k < a[0].length; k++) 
+            {
+                sum[i][j] = a[i][k] * b[k][j];
+            }
         }
     }
     console.log(sum);
