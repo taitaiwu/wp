c1e5abe203dd5ceec1c49ec90bbc6802ee70daf2
taitaiwu <s919075@stu.chsc.tw>
2024-06-16 04:03:10 +0800

    Homework11 update main.js

--- Homework11/denoApiService/blog/main.js
+++ Homework11/denoApiService/blog/main.js
@@ -8,45 +8,45 @@ window.onhashchange = async function ()
 {
   var r
 
-  var tokens = window.location.hash.split('/')
-  console.log('tokens=', tokens)
+  var tokens = window.location.hash.split('/')  //辨識當前網址，並用split將網址斜線的地方分割城串列
+  console.log('tokens=', tokens)  //輸出tokens(串列)的值
 
-  switch (tokens[0]) 
+  switch (tokens[0])  //分析第一筆傳入
   {
     case '#show':
-      let r = await sqlFetch('blog', `SELECT id, title, body FROM posts WHERE id=${tokens[1]}`)
-      R.show(r[0]) // 取得第一筆傳入 (雖然只會有一筆，但 SELECT 預設會傳回很多比，所以用 results[0] 只取第一筆)
+      let r = await sqlFetch('blog', `SELECT id, title, body FROM posts WHERE id=${tokens[1]}`)  //r = 將blog的資料放入sqlFetch函式(在sql.js檔案)結果
+      R.show(r[0])  //取得第一筆傳入 (雖然只會有一筆，但 SELECT 預設會傳回很多筆，所以用 results[0] 只取第一筆)
       break
 
     case '#new':
-      R.new()
+      R.new()  //更新R串列
       break
 
     default:
-      let posts = await sqlFetch('blog', `SELECT id, title, body FROM posts`)
-      R.list(posts)
+      let posts = await sqlFetch('blog', `SELECT id, title, body FROM posts`)  //post = 將blog的資料放入sqlFetch(在sql.js檔案)函式結果
+      R.list(posts)  //R加入posts
       break
   }
 }
 
 window.onload = async function () 
 {
-  await sqlFetch('blog', `CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, body TEXT)`)
+  await sqlFetch('blog', `CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, body TEXT)`)  //將blog的資料放入sqlFetch(在sql.js檔案)函式
   window.onhashchange()
 }
 
-R.layout = function (title, content) 
+R.layout = function (title, content)  //把標題和留言放進函式
 {
-  document.querySelector('title').innerText = title
-  document.querySelector('#content').innerHTML = content
+  document.querySelector('title').innerText = title  //加入標題
+  document.querySelector('#content').innerHTML = content  //加入留言
 }
 
-R.list = function (posts) 
+R.list = function (posts)   //把貼文放入函式
 {
-  let list = []
+  let list = []  //設定一個空的list串列
 
-  for (let post of posts) 
-    {
+  for (let post of posts)  //一個經歷posts的迴圈
+  {
     list.push
     (`
       <li>
